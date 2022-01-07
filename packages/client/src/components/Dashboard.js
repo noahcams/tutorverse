d7ab85db1f90a618dc6ffd925040e7e83bb2845f
Noah Camara <noahcmr@gmail.com>
2022-01-07 07:12:38 -0500

    Fixed the dashboard issue

--- packages/client/src/components/Dashboard.js
+++ packages/client/src/components/Dashboard.js
@@ -1,63 +1,61 @@
-import React, { useState, useEffect } from 'react'
-import { Container,
-  Button,
-  ListGroup,
-  Row,
-  Col } from 'react-bootstrap'
-import { useParams } from 'react-router-dom'
-import StudentDetails from './StudentDetails.js'
-import TeacherDetails from './TeacherDetails.js'
-import AssignmentList from './AssignmentList.js'
-import axios from 'axios'
+import React, { useState, useEffect } from 'react';
+import { Container, Button, ListGroup, Row, Col } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
+import StudentDetails from './StudentDetails.js';
+import TeacherDetails from './TeacherDetails.js';
+import AssignmentList from './AssignmentList.js';
+import axios from 'axios';
 
 export default function Dashboard({ user }) {
-  const [cls, setCls] = useState([])
-  
-  // get classId from URL
-  // This was throwing an error for me. I think the issue is user.classIds is an array. -Noah
-  let { id } = useParams()
-  let getDashboard = async (id) => {
-    try {
-      const classes = await axios.get(`http://localhost:3001/classes/${user.classIds}`)
-      setCls(classes.data)
-      if(user.type === "teacher"){
-        const students = await axios.get(`http://localhost:3001/users/${cls.students}`)
-        console.log (students)
-      }
-    } catch (err) {
-      console.error(err)
-    }
-  }
-  
-  useEffect(() => {
-    getDashboard(id)
-  },[])
+	const [cls, setCls] = useState([]);
 
-  // console.log(user)
-  // console.log(cls.students)
+	// get classId from URL
+	// This was throwing an error for me. I think the issue is user.classIds is an array. -Noah
+	let { id } = useParams();
+	let getDashboard = async (id) => {
+		//
+		try {
+			const classes = user.classIds.map(
+				async (cl) =>
+					await (await axios.get(`http://localhost:3001/classes/${cl}`)).data
+			)
+			setCls(classes);
+			if (user.type === 'teacher') {
+				const students = await axios.get(
+					`http://localhost:3001/users/${cls.students}`
+				);
+				console.log(students);
+			}
+		} catch (err) {
+			console.error(err);
+		}
+	};
 
-  
-  return (
-    <div>
-      <Container className="dashPage">
-        <Row id="dashContent">
+	useEffect(() => {
+		getDashboard(id);
+	}, []);
 
-          {
-          user.type === 'student' &&
-            <ListGroup id="assignmentList">
-            <h2>Assignment List</h2>
-            <AssignmentList props={cls} />
-            </ListGroup> 
-          }
+	// console.log(user)
+	// console.log(cls.students)
 
-          {
-          user.type === 'teacher' &&
-            <ListGroup id='studentList'>
-              <ListGroup.Item>Hello</ListGroup.Item>
-            </ListGroup>
-          }
-        </Row>
-    </Container>
-    </div>
-  )
+	return (
+		<div>
+			<Container className="dashPage">
+				<Row id="dashContent">
+					{user.type === 'student' && (
+						<ListGroup id="assignmentList">
+							<h2>Assignment List</h2>
+							{cls.map((cl) => <AssignmentList props={cl} />)}
+						</ListGroup>
+					)}
+
+					{user.type === 'teacher' && (
+						<ListGroup id="studentList">
+							<ListGroup.Item>Hello</ListGroup.Item>
+						</ListGroup>
+					)}
+				</Row>
+			</Container>
+		</div>
+	);
 }
