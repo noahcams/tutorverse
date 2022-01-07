a1b1714b59d466932a7a95bf78ae6070dd6bbdae
Noah Camara <noahcmr@gmail.com>
2022-01-06 21:38:46 -0500

    added a comment

--- packages/client/src/components/ClassDetails.js
+++ packages/client/src/components/ClassDetails.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Card } from 'react-bootstrap';
 
 export default function Class({ cls }) {
+  //get teacher's name or change model to use teacher string rather than object id...
+
   return (
     <Card>
       <Card.Header>
