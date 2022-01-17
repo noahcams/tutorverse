c90e1b606e46924b89d4f720ab2732f2610bb9c8
Noah Camara <noahcmr@gmail.com>
2022-01-17 12:19:49 -0500

    Added a loading spinner to the log in button

--- packages/client/src/components/Login.js
+++ packages/client/src/components/Login.js
@@ -2,19 +2,24 @@ import { useState } from 'react';
 import { Form, Button, Container, Row, Col } from 'react-bootstrap';
 import axios from 'axios';
 import { toast } from 'react-toastify';
+import { Oval } from 'react-loader-spinner';
 
 export default function Login({ setUser, setLoggedIn}) {
 	const [username, setUsername] = useState('');
 	const [password, setPassword] = useState('');
+	const [isLoading, setIsLoading] = useState(false);
 
 	const handleLogin = async (e) => {
 		e.preventDefault();
+		setIsLoading(true);
 		try {
 			const res = await axios.post('http://localhost:3001/auth/signin', { username, password });
 			setUser(res.data);
+			setIsLoading(false);
 			setLoggedIn(true);
 			localStorage.setItem('user', JSON.stringify(res.data));
 		} catch (error) {
+			setIsLoading(false);
 			toast.error('Invalid username or password');
 			toast.error(error);
 		}
@@ -38,7 +43,10 @@ export default function Login({ setUser, setLoggedIn}) {
 										onChange={(e) => setUsername(e.target.value)}
 									/>
 								</Form.Group>
-								<Form.Group className="user-password" controlId="formLogInPassword">
+								<Form.Group
+									className="user-password"
+									controlId="formLogInPassword"
+								>
 									<Form.Label>Password</Form.Label>
 									<Form.Control
 										type="password"
@@ -48,7 +56,16 @@ export default function Login({ setUser, setLoggedIn}) {
 									/>
 								</Form.Group>
 								<Button variant="primary" type="submit">
-									Log In
+									{isLoading ? (
+										<Oval
+											height="20"
+											width="20"
+											color="white"
+											arialLabel="loading"
+										/>
+									) : (
+										'Log In'
+									)}
 								</Button>
 							</Form>
 						</Col>
